9af4e6712794765f9d0128cd9597d549dd114cf1
Rebell <dmarszk@gmail.com>
2012-08-21 01:29:09 +0200

    More GCC errors fixes.

--- kernel/drivers/media/video/samsung/tv20/s5p_stda_grp.c
+++ kernel/drivers/media/video/samsung/tv20/s5p_stda_grp.c
@@ -598,6 +598,7 @@ int s5ptvfb_set_par(struct fb_info *fb)
 {
 	struct s5ptvfb_window *win = fb->par;
 
+	
 	dev_dbg(s5ptv_status.dev_fb, "[fb%d] set_par\n", win->id);
 
 	if (!fb->fix.smem_start) {
@@ -609,8 +610,7 @@ int s5ptvfb_set_par(struct fb_info *fb)
 		"[Warning] The frame buffer should be allocated by ioctl\n");
 #endif
 	}
-
-	((struct fb_var_screeninfo) (s5ptv_status.fb->var)).bits_per_pixel = ((struct fb_var_screeninfo) (fb->var)).bits_per_pixel;
+	s5ptv_status.fb->var.bits_per_pixel = fb->var.bits_per_pixel;
 
 	s5ptvfb_set_display_mode(&s5ptv_status);
 
